5c3d1e01237b8feb083b8d525d0d3068b2b0a31e
Karl Dixon <karlandrewdixon@gmail.com>
2024-03-07 16:23:26 +0000

    ADD: Post details

--- hackathon3/urls.py
+++ hackathon3/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
 path('',views.index, name='index'),
-path('post/', views.PostList.as_view(), name='posts')
+path('post/', views.PostList.as_view(), name='posts'),
+path('profile/', views.profile, name="profile"),
+path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
