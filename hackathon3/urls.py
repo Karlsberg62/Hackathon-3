6a9d1ab24ce6623d3ca73a32f81212dd3017d26e
Ellis Taylor <ellis.taylor@live.com>
2024-03-07 22:25:34 +0000

    added link to update button

--- hackathon3/urls.py
+++ hackathon3/urls.py
@@ -7,5 +7,6 @@ path('post/', views.PostList.as_view(), name='posts'),
 path('profile/', views.profile, name="profile"),
 path('<slug:slug>/', views.post_detail, name='post_detail'),
 path('make_event', views.make_event.as_view(), name='make-event'),
-path('<slug:slug>/update/', views.update_event.as_view(), name='update_event')
+path('<slug:slug>/update/', views.update_event.as_view(), name='update_event'),
+# path('<slug:slug>/delete/', views.delete_event.as_view(), name='delete_event'),
 ]
