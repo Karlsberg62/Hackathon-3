c63d28b0d85aafdc236c4b56b9de17d0b46f3d2d
Ellis Taylor <ellis.taylor@live.com>
2024-03-07 19:17:17 +0000

    sorted nav links, no url for clubs yet

--- hackathon3/urls.py
+++ hackathon3/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('',views.index, name='index'),
+path('',views.index, name='home'),
 path('post/', views.PostList.as_view(), name='posts'),
 path('profile/', views.profile, name="profile"),
 path('<slug:slug>/', views.post_detail, name='post_detail'),
