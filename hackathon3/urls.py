9219e2408bf10f718e5b07b8001ad155dc6291e7
Ellis Taylor <ellis.taylor@live.com>
2024-03-07 16:43:13 +0000

    added post_detail view/url and added link to post

--- hackathon3/urls.py
+++ hackathon3/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 path('',views.index, name='index'),
-path('post/', views.PostList.as_view(), name='posts')
+path('post/', views.PostList.as_view(), name='posts'),
+path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- hackathon3/views.py
+++ hackathon3/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from .models import Post
 # Create your views here.
@@ -7,7 +7,7 @@ def index(request):
 
 class PostList(generic.ListView):
     """
-    Returns all published posts in :model:`blog.Post`
+    Returns all published posts in :model:`hackathon3.Post`
     and displays them in a page of six posts. 
     **Context**
 
@@ -24,3 +24,38 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "hackathon3/post.html"
     paginate_by = 4
+
+
+def post_detail(request, slug):
+    """
+     Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comments``
+        All approved comments related to the post.
+    ``comment_count``
+        A count of approved comments related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+   
+    
+    
+
+    return render(
+        request,
+        "hackathon3/post_detail.html",
+        {"post": post,
+        
+        }
+    )    
