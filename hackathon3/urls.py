c7201d50ea4374811e955e09b2ad2a95b5f4d9dc
Ben Fashan <benfashan71@msn.com>
2024-03-07 22:55:00 +0000

    MAIT: make_event view url name made inline

--- hackathon3/urls.py
+++ hackathon3/urls.py
@@ -6,6 +6,6 @@ path('',views.index, name='home'),
 path('post/', views.PostList.as_view(), name='posts'),
 path('profile/', views.profile, name="profile"),
 path('<slug:slug>/', views.post_detail, name='post_detail'),
-path('make_event', views.make_event.as_view(), name='make-event'),
+path('make_event', views.make_event.as_view(), name='make_event'),
 path('<slug:slug>/update/', views.update_event.as_view(), name='update_event')
 ]
