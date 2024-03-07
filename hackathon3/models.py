33c4a30b0a8702da6906f5693420595a4e6e0fbd
Ben Fashan <benfashan71@msn.com>
2024-03-07 11:12:41 +0000

    ADD: model and views for post and post details

--- hackathon3/models.py
+++ hackathon3/models.py
@@ -10,11 +10,17 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='event_posts')
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    time = models.TimeField()
+    location = models.CharField(max_length=200)
+    max_attendees = models.IntegerField()
     featured_image = CloudinaryField('image', default='placeholder')
-    content = models.TextField()
+    image_alt_text = models.CharField(max_length=200, default='placeholder')
+    max_attendees = models.IntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.BooleanField(default=False)  
-    update_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)  
+    updated_on = models.DateTimeField(auto_now=True)
      
 
     class Meta:
@@ -24,6 +30,20 @@ class Post(models.Model):
 
 
 
+class Comment(models.Model):
+    """
+    stores a single comment entry related to the :model:'auth.User' :model:'blog.Post'
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)   
+
+    class Meta:
+        ordering = ['created_on']
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
 
 
 
--- hackathon3/views.py
+++ hackathon3/views.py
@@ -1,5 +1,84 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.contrib import messages
+from .models import Post 
+from .forms import CommentForm
 
 # Create your views here.
 def index(request):
     return render(request,'hackathon3/index.html')
+
+# Create your views here.
+# class based view
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1)
+    template_name = "hackathon3/index.html"
+    paginate_by = 4
+        #searchbar functionality
+    def get_queryset(self, **kwargs):
+        query = self.request.GET.get('q')
+        if query:
+            post = self.model.objects.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query) |
+                Q(date__icontains=query) |
+                Q(location__icontains=query)
+            )
+        else:
+            post = self.model.objects.all()
+        return post
+
+
+
+## function based view
+def PostDetail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.   
+     ``comments``
+        All approved comments related to the post.
+    ``comment_count``
+        A count of approved comments related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    # this is a shortcut to get data or raise a https404error # same as render being a shortcut to load data to a templ and return it. 
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comment.all().order_by("-created_on")
+    comment_count = post.description.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+    )
+
+    comment_form = CommentForm()
+    
+
+    return render(
+        request,
+        "hackathon3/post_detail.html",
+        {
+            "post": post, 
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
+    )
