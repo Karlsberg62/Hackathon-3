16b42bbba81782370c7deb9582ada03424e02bb5
Karl Dixon <karlandrewdixon@gmail.com>
2024-03-07 20:55:00 +0000

    ADD: Additional functionality for comments and notification

--- hackathon3/views.py
+++ hackathon3/views.py
@@ -8,8 +8,9 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
 from .models import Post
-from .forms import EventForm
+from .forms import EventForm, CommentForm
 
 # Create your views here.
 def index(request):
@@ -75,7 +76,19 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
-    
+    comment_form = CommentForm()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
     
 
     return render(
@@ -84,6 +97,7 @@ def post_detail(request, slug):
         {"post": post,
         "comments": comments,
         "comment_count": comment_count,
+        "comment_form": comment_form,
         }
     )    
 
