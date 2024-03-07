4ec99d1774e9fc470dfd0c358dca1d4590fc7bb1
Ellis Taylor <ellis.taylor@live.com>
2024-03-07 14:41:31 +0000

    added summernote and autofill to slug/title

--- hackathon3/models.py
+++ hackathon3/models.py
@@ -8,21 +8,25 @@ class Post(models.Model):
     stores a single event post entry related to the :model:'auth.User'.
     """
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='event_posts')
     category = models.CharField(max_length=200)
-    description = models.TextField()
-    date = models.DateTimeField()
+    description = models.TextField(blank=True)
+    excerpt = models.TextField(max_length=500,blank=True)
+    date = models.DateField()
     time = models.TimeField()
     location = models.CharField(max_length=200)
     max_attendees = models.IntegerField()
     featured_image = CloudinaryField('image', default='placeholder')
     image_alt = models.CharField(max_length=200, default='placeholder')
-    max_attendees = models.IntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=False)  
     updated_on = models.DateTimeField(auto_now=True)
      
+    def save(self, *args, **kwargs):
+        if not self.slug:  # Generate slug only if it's not already set
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ['-created_on']
